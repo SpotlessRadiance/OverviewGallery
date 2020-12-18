10ead8023dbfdb05766906ee856de290ef6d2098
SpotlessRadiance <55090374+SpotlessRadiance@users.noreply.github.com>
2020-12-18 22:55:19 +0300

    Update Image.java

--- src/main/java/gallery/model/Image.java
+++ src/main/java/gallery/model/Image.java
@@ -40,46 +40,8 @@ public class Image
         this.author = author;
     }
 
-   /* @Column(name = "height")
-    private Integer height;
-
-    @Column(name = "width")
-    private Integer width;*/
-
 
     private float[] features;
-
-    /*@ManyToOne
-    @JsonIgnoreProperties("")
-    private Album album;//игнорируем при десереализации
-
-    @ManyToMany
-    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-    @JoinTable(name = "photo_tag",
-               joinColumns = @JoinColumn(name = "photos_id", referencedColumnName = "id"),
-               inverseJoinColumns = @JoinColumn(name = "tags_id", referencedColumnName = "id"))
-    private Set<Tag> tags = new HashSet<>();
-
-    public Set<Tag> getTags() {
-        return tags;
-    }
-
-    public Photo tags(Set<Tag> tags) {
-        this.tags = tags;
-        return this;
-    }
-
-    public Photo addTag(Tag tag) {
-        this.tags.add(tag);
-        tag.getPhotos().add(this);
-        return this;
-    }
-
-    public Photo removeTag(Tag tag) {
-        this.tags.remove(tag);
-        tag.getPhotos().remove(this);
-        return this;
-    }*/
 
     public Image(){}
 
@@ -106,22 +68,6 @@ public class Image
         this.title=title;
     }
 
-   /* public int getHeight(){
-        return height;
-    }
-
-    public void setHeight(int height){
-        this.height = height;
-    }
-
-    public int getWidth(){
-        return width;
-    }
-
-    public void setWidth(int width){
-        this.width = width;
-    }*/
-
     public byte[] getImage() {
         return image;
     }
@@ -144,22 +90,8 @@ public class Image
         return "Image{" + "  title='" + title + '\'' + ", blob='" + image + '\'' + '}';
     }
 
-    /*@Override
-    public String toString() {
-        return "Photo{" +
-                "id=" + getId() +
-                ", title='" + getTitle() + "'" +
-                ", image='" + getImage() + "'" +
-                // ", imageContentType='" + getImageContentType() + "'" +
-                ", height=" + getHeight() +
-                ", width=" + getWidth() +
-                //   ", taken='" + getTaken() + "'" +
-                //w    ", uploaded='" + getUploaded() + "'" +
-                "}";
-    }*/
-
         public String to64encode() {
-            return Base64.encodeBase64String(this.image); //картинка в виде большой длинной какахи
+            return Base64.encodeBase64String(this.image); 
         }
     }
 
